2bda8bfffee26a7876093f54de0c070736117ef3
MathewsAvlis <mathewsavlis@gmail.com>
2026-09-02 20:07:04 -0300

    Correção das imagens que não apareciam no about

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -5,11 +5,11 @@ export const aboutData = {
   paragraph: "Meu trabalho nasce do encontro entre quem está diante da minha câmera e a forma como eu a enxergo. Mais do que criar uma imagem bonita, busco perceber gestos, força, delicadeza e características que muitas vezes passam despercebidas no dia a dia.",
   images: {
     photo1: {
-      src: "/foto-sobre-1.png",
+      src: "/landingpage-byspozzer/lafoto-sobre-1.png",
       alt: "Sthefani Pozzer em ação durante uma sessão fotográfica"
     },
     photo2: {
-      src: "/foto-sobre-2.png",
+      src: "/landingpage-byspozzer/foto-sobre-2.png",
       alt: "Retrato que reflete a essência e o olhar de Sthefani Pozzer"
     }
   }
