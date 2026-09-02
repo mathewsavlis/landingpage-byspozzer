d5f7d08ae43359a5deecdf6cc348d464581c67bc
MathewsAvlis <mathewsavlis@gmail.com>
2026-09-02 19:41:55 -0300

    Configura exportacao estatica do Next

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.2.129'],
   reactCompiler: true,
+  output: 'export', // Diz ao Next para gerar uma pasta HTML/CSS estática
+  basePath: '/landingpage-byspozzer', // Substitua pelo nome exato do seu repositório no GitHub
+  images: {
+    unoptimized: true, // Necessário se você usa a tag <Image /> do Next
+  }
 };
 
 export default nextConfig;
